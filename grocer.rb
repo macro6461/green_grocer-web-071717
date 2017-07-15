1a88c7144c42e7bdf29cf004e3081da125861d35
Matthew Croak <mattcroak718@gmail.com>
2017-07-15 17:03:33 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,17 @@
 def consolidate_cart(cart)
   # code here
+  count = 1
+  cart_hash = Hash[cart.collect { |item| [item, ""] }]
+  cart_hash.each do |item, value|
+    item.each do |attributes, count|
+      cart_hash[attributes][:count] = 1
+      if
+        count += 1
+      else
+        count
+      end
+    end
+  end
 end
 
 def apply_coupons(cart, coupons)
@@ -8,8 +20,10 @@ end
 
 def apply_clearance(cart)
   # code here
+  cart.each do
 end
 
 def checkout(cart, coupons)
   # code here
+
 end
